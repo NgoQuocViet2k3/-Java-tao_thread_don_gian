9bf96a2d9035c7d67b77422524136128fa86dad4
ruukichi <ngovietlc93@gmail.com>
2022-01-27 16:42:43 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,15 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    NumberGenerator num1 = new NumberGenerator();
+        NumberGenerator num2 = new NumberGenerator();
+
+        Thread thread1 = new Thread(num1);
+        Thread thread2 = new Thread(num2);
+        thread2.setPriority(Thread.MAX_PRIORITY);
+        thread1.start();
+        thread2.start();
+    }
+}
